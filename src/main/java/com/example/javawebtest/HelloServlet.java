0cffa36a924f7f6f2177175467adef9a7725d033
VictorRemotes <victor.chen@remotes.com.tw>
2021-12-21 10:22:44 +0800

    2.0

--- src/main/java/com/example/javawebtest/HelloServlet.java
+++ src/main/java/com/example/javawebtest/HelloServlet.java
@@ -7,9 +7,12 @@ import javax.servlet.annotation.*;
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
+    int number;
+
 
     public void init() {
         message = "Hello World!";
+        number =0;
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -23,5 +26,6 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void destroy() {
+        System.out.println("Hello Word");
     }
 }
