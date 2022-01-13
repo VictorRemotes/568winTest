e8fbe5ed1d6be02cf853a91f67786d77df25f7c8
VictorRemotes <victor.chen@remotes.com.tw>
2022-01-13 15:42:53 +0800

    Reset

--- src/main/resources/start_hub.bat
+++
@@ -1,4 +0,0 @@
-c:
-cd c:\Users\Dee\SeleniumServer\
-java -jar selenium-server-standalone-3.11.0.jar -role hub
-pause
--- test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Press any key to start
-pause
-echo Executing...
-Java -cp bin;lib/* org.testng.TestNG testng.xml
-echo Press any key to end
-pause
-exit
