6caeaa7f3fbda6371bc56a1be92d2f5d6ede824e
VictorRemotes <victor.chen@remotes.com.tw>
2022-03-04 17:57:59 +0800

    0304

--- 568winTest/UnitTest1.cs
+++ 568winTest/UnitTest1.cs
@@ -61,8 +61,10 @@ namespace MSTest_Web_Test
             driver.FindElement(By.Id("password")).Clear();
             driver.FindElement(By.Id("password")).SendKeys("123456");
             driver.FindElement(By.Id("prevent-btn-sign-in")).Click();
-            Console.WriteLine("----------Test for entering wrong password----------");
+            Console.WriteLine("---------- Test for entering wrong password ----------");
+
             Thread.Sleep(5000);
+
             driver.FindElement(By.Id("username")).Click();
             driver.FindElement(By.Id("username")).Clear();
             driver.FindElement(By.Id("username")).SendKeys("testnick");
@@ -70,31 +72,86 @@ namespace MSTest_Web_Test
             driver.FindElement(By.Id("password")).Clear();
             driver.FindElement(By.Id("password")).SendKeys("1234asdf");
             driver.FindElement(By.Id("prevent-btn-sign-in")).Click();
-            Console.WriteLine("----------Test entering correct password----------");
+            Console.WriteLine("---------- Test entering correct password ----------");
 
             driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2243yy_logo_5f869bfc8ba7f_desktop.png?v=202201232224')]")).Click();
-            Console.WriteLine("----------Test whether clicking on the title can return to the home page----------");
+            Console.WriteLine("---------- Test whether clicking on the title can return to the home page ----------");
 
             driver.FindElement(By.XPath("//a[@id='home']/div[2]/i")).Click();
-            Console.WriteLine("----------Test click on Sports field----------");
+            Console.WriteLine("---------- Test click on Home ----------");
 
             driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Deposit & Withdrawal'])[1]/following::span[3]")).Click();
-            Console.WriteLine("----------Test click on Virtual Sports field----------");
+            Console.WriteLine("---------- Test click on Deposit & Withdrawal ----------");
 
             driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Sports'])[1]/following::span[1]")).Click();
-            Console.WriteLine("----------Test click on Live Casino field----------");
+            Console.WriteLine("---------- Test click on Sports ----------");
 
             driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Virtual Sports'])[1]/following::span[1]")).Click();
-            Console.WriteLine("----------Test click on Games field----------");
+            Console.WriteLine("---------- Test click on Virtual Sports ----------");
 
             driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Live Casino'])[1]/following::span[1]")).Click();
-            Console.WriteLine("----------Test click on CockFighting field----------");
+            Console.WriteLine("---------- Test click on Live Casino ----------");
 
             driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Games'])[1]/following::span[1]")).Click();
-            Console.WriteLine("----------Test click on Sports field----------");
-        }
+            Console.WriteLine("---------- Test click on Games ----------");
 
-        private bool IsElementPresent(By by)
+            //回到首頁
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2243yy_logo_5f869bfc8ba7f_desktop.png?v=202201232224')]")).Click();
+            
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2243yy_banner_1_5f8696ae715ce_001.png?v=202201232224')]")).Click();
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2243yy_logo_5f869bfc8ba7f_desktop.png?v=202201232224')]")).Click();
+
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2243yy_banner_2_5f8696b2ab1d0_002.png?v=202201232224')]")).Click();
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2243yy_logo_5f869bfc8ba7f_desktop.png?v=202201232224')]")).Click();
+
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2274yy_banner_3_5fbce52ac838d_demobsibanner.png?v=202201232224')]")).Click();
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2243yy_logo_5f869bfc8ba7f_desktop.png?v=202201232224')]")).Click();
+
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2274yy_banner_4_61a8996a5761b_sbolive_bg_en_m.png?v=202201232224')]")).Click();
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2243yy_logo_5f869bfc8ba7f_desktop.png?v=202201232224')]")).Click();
+
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2243yy_banner_5_5f8696bbec0da_005.png?v=202201232224')]")).Click();
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2243yy_logo_5f869bfc8ba7f_desktop.png?v=202201232224')]")).Click();
+
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2243yy_banner_6_5f8696be5af35_006.png?v=202201232224')]")).Click();
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2243yy_logo_5f869bfc8ba7f_desktop.png?v=202201232224')]")).Click();
+
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2274yy_information_banner_1_61a89d51e5388_bsd3_information_banner_1.png?v=202201232224')]")).Click();
+            driver.FindElement(By.XPath("//img[contains(@src,'https://img-1-10.cdn568.net/storage/customize/WebId-2274/Blue_SBO_Desktop_3/2243yy_logo_5f869bfc8ba7f_desktop.png?v=202201232224')]")).Click();
+            Console.WriteLine("--------- Homepage image link test ----------");
+
+            driver.FindElement(By.XPath("//div[@id='content-hover-loader-SPORTS']/div/a/div/div/img")).Click();
+            Console.WriteLine("---------- click SBO SPORTS ----------");
+            Thread.Sleep(5000);
+            driver.FindElement(By.XPath("//div[@id='content-hover-loader-SPORTS']/div/a[2]/div/div/img")).Click();
+            Console.WriteLine("----------click Saba Sports LOBBY ----------");
+            driver.FindElement(By.XPath("//div[@id='content-hover-loader-SPORTS']/div/a[3]/div/div/img")).Click();
+            Console.WriteLine("----------click RCS ----------");
+            driver.FindElement(By.XPath("//div[@id='content-hover-loader-VIRTUAL_SPORTS']/div/a/div/div/img")).Click();
+            Console.WriteLine("---------- click SBO Virtual Sports ----------");
+
+
+            driver.FindElement(By.XPath("//img[@alt='GiocoPlus']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='SBOBET']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='Gamatron']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='Huawei4D']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='RedTiger']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='FlowGaming']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='PGSoft']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='Yggdrasil']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='FunkyGames']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='SBOSlots']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='WorldMatch']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='RealTimeGaming']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='JokerGaming']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='PragmaticPlay']")).Click();
+            driver.FindElement(By.XPath("//img[@alt='CQNine']")).Click();
+            driver.FindElement(By.XPath("//div[@id='content-hover-loader-COCK_FIGHT']/div/a/div/div/img")).Click();
+            Console.WriteLine("");
+        }
+       
+
+            private bool IsElementPresent(By by)
         {
             try
             {
